0cf2fe3a5e9a57c421355800a1f809fb6bdd39c7
Manuel González Segura <gonzalez.segura.manuel@gmail.com>
2020-02-06 18:39:03 +0100

    Added bootstrap template to project ASP.NET Razor

---
+++ ManzanosDentalClinic/Pages/About.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Localization;
+
+namespace ManzanosDentalClinic
+{
+    public class AboutModel : PageModel
+    {        
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
